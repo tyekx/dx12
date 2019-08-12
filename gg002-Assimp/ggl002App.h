aec65828933b0b14188671976895e2de1b74a34e
Musa Benedek <musa.bence@gmail.com>
2019-08-12 14:38:23 +0200

    ggl002-start

--- gg002-Assimp/ggl002App.h
+++ gg002-Assimp/ggl002App.h
@@ -4,7 +4,6 @@
 #include <Egg/Importer.h>
 #include <Egg/Math/Math.h>
 #include <Egg/ConstantBuffer.hpp>
-
 #include "ConstantBufferTypes.h"
 
 using namespace Egg::Math;
@@ -12,15 +11,9 @@ using namespace Egg::Math;
 class ggl002App : public Egg::SimpleApp {
 protected:
 	Egg::Mesh::Shaded::P shadedMesh;
-	com_ptr<ID3D12Resource> depthStencilBuffer;
-	com_ptr<ID3D12DescriptorHeap> dsvHeap;
-	Float4x4 rotation;
-	Egg::ConstantBuffer<PerObjectCb> cb;
 public:
 	virtual void Update(float dt, float T) override {
-		rotation *= Float4x4::Rotation(Float3::UnitY, dt);
-		cb->modelTransform = Float4x4::Translation(Float3{ 0.0f, 0.0f, -0.5f }) * rotation * Float4x4::Translation(Float3{ 0.0f, 0.0f, 0.5f });
-		cb.Upload();
+
 	}
 
 	virtual void PopulateCommandList() {
@@ -33,15 +26,12 @@ public:
 		commandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(renderTargets[frameIndex].Get(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET));
 
 		CD3DX12_CPU_DESCRIPTOR_HANDLE rHandle(rtvDescriptorHeap->GetCPUDescriptorHandleForHeapStart(), frameIndex, rtvDescriptorHandleIncrementSize);
-		CD3DX12_CPU_DESCRIPTOR_HANDLE dsvHandle(dsvHeap->GetCPUDescriptorHandleForHeapStart());
-		commandList->OMSetRenderTargets(1, &rHandle, FALSE, &dsvHandle);
+		commandList->OMSetRenderTargets(1, &rHandle, FALSE, nullptr);
 
 		const float clearColor[] = { 0.0f, 0.2f, 0.4f, 1.0f };
 		commandList->ClearRenderTargetView(rHandle, clearColor, 0, nullptr);
-		commandList->ClearDepthStencilView(dsvHandle, D3D12_CLEAR_FLAG_DEPTH, 1.0f, 0, 0, nullptr);
 
 		shadedMesh->SetPipelineState(commandList.Get());
-		shadedMesh->BindConstantBuffer(commandList.Get(), cb);
 		shadedMesh->Draw(commandList.Get());
 
 		commandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(renderTargets[frameIndex].Get(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT));
@@ -50,53 +40,8 @@ public:
 			commandList->Close();
 	}
 
-	virtual void CreateSwapChainResources() override {
-		Egg::SimpleApp::CreateSwapChainResources();
-
-		D3D12_DESCRIPTOR_HEAP_DESC dsHeapDesc = {};
-		dsHeapDesc.NumDescriptors = 1;
-		dsHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_DSV;
-		dsHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
-		
-		DX_API("Failed to create depth stencil descriptor heap")
-			device->CreateDescriptorHeap(&dsHeapDesc, IID_PPV_ARGS(dsvHeap.GetAddressOf()));
-
-
-		D3D12_CLEAR_VALUE depthOptimizedClearValue = {};
-		depthOptimizedClearValue.Format = DXGI_FORMAT_D32_FLOAT;
-		depthOptimizedClearValue.DepthStencil.Depth = 1.0f;
-		depthOptimizedClearValue.DepthStencil.Stencil = 0;
-
-		DX_API("Failed to create Depth Stencil buffer")
-			device->CreateCommittedResource(
-				&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
-				D3D12_HEAP_FLAG_NONE,
-				&CD3DX12_RESOURCE_DESC::Tex2D(DXGI_FORMAT_D32_FLOAT, scissorRect.right, scissorRect.bottom, 1, 0, 1, 0, D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL),
-				D3D12_RESOURCE_STATE_DEPTH_WRITE,
-				&depthOptimizedClearValue,
-				IID_PPV_ARGS(depthStencilBuffer.GetAddressOf()));
-
-		depthStencilBuffer->SetName(L"Depth Stencil Buffer");
-
-		D3D12_DEPTH_STENCIL_VIEW_DESC depthStencilDesc = {};
-		depthStencilDesc.Format = DXGI_FORMAT_D32_FLOAT;
-		depthStencilDesc.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2D;
-		depthStencilDesc.Flags = D3D12_DSV_FLAG_NONE;
-
-		device->CreateDepthStencilView(depthStencilBuffer.Get(), &depthStencilDesc, dsvHeap->GetCPUDescriptorHandleForHeapStart());
-	}
-
-	virtual void ReleaseSwapChainResources() override {
-		depthStencilBuffer.Reset();
-		dsvHeap.Reset();
-
-		Egg::SimpleApp::ReleaseSwapChainResources();
-	}
-
 	virtual void LoadAssets() override {
-		cb.CreateResources(device.Get());
-
-		com_ptr<ID3DBlob> vertexShader = Egg::Shader::LoadCso("Shaders/cbBasicVS.cso");
+		com_ptr<ID3DBlob> vertexShader = Egg::Shader::LoadCso("Shaders/DefaultVS.cso");
 		com_ptr<ID3DBlob> pixelShader = Egg::Shader::LoadCso("Shaders/DefaultPS.cso");
 		com_ptr<ID3D12RootSignature> rootSig = Egg::Shader::LoadRootSignature(device.Get(), vertexShader.Get());
 
@@ -104,8 +49,6 @@ public:
 		material->SetRootSignature(rootSig);
 		material->SetVertexShader(vertexShader);
 		material->SetPixelShader(pixelShader);
-		material->SetDepthStencilState(CD3DX12_DEPTH_STENCIL_DESC(D3D12_DEFAULT));
-		material->SetDSVFormat(DXGI_FORMAT_D32_FLOAT);
 
 		Egg::Mesh::Geometry::P geometry = Egg::Importer::ImportSimpleObj(device.Get(), "C:/work/dx12/Media/giraffe.obj");
 
