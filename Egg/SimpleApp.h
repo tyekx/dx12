bf69f3b7192d0e93f9b398a6f6b4e256c2992a07
Musa Benedek <musa.bence@gmail.com>
2019-08-12 14:56:13 +0200

    gg003 init

--- Egg/SimpleApp.h
+++ Egg/SimpleApp.h
@@ -11,6 +11,8 @@ namespace Egg {
 	protected:
 		com_ptr<ID3D12CommandAllocator> commandAllocator;
 		com_ptr<ID3D12GraphicsCommandList> commandList;
+		com_ptr<ID3D12Resource> depthStencilBuffer;
+		com_ptr<ID3D12DescriptorHeap> dsvHeap;
 
 		std::unique_ptr<PsoManager> psoManager;
 
@@ -71,6 +73,48 @@ namespace Egg {
 			fence.Reset();
 			commandAllocator.Reset();
 			App::ReleaseResources();
+		}
+
+		virtual void CreateSwapChainResources() override {
+			Egg::App::CreateSwapChainResources();
+
+			D3D12_DESCRIPTOR_HEAP_DESC dsHeapDesc = {};
+			dsHeapDesc.NumDescriptors = 1;
+			dsHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_DSV;
+			dsHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+
+			DX_API("Failed to create depth stencil descriptor heap")
+				device->CreateDescriptorHeap(&dsHeapDesc, IID_PPV_ARGS(dsvHeap.GetAddressOf()));
+
+
+			D3D12_CLEAR_VALUE depthOptimizedClearValue = {};
+			depthOptimizedClearValue.Format = DXGI_FORMAT_D32_FLOAT;
+			depthOptimizedClearValue.DepthStencil.Depth = 1.0f;
+			depthOptimizedClearValue.DepthStencil.Stencil = 0;
+
+			DX_API("Failed to create Depth Stencil buffer")
+				device->CreateCommittedResource(
+					&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+					D3D12_HEAP_FLAG_NONE,
+					&CD3DX12_RESOURCE_DESC::Tex2D(DXGI_FORMAT_D32_FLOAT, scissorRect.right, scissorRect.bottom, 1, 0, 1, 0, D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL),
+					D3D12_RESOURCE_STATE_DEPTH_WRITE,
+					&depthOptimizedClearValue,
+					IID_PPV_ARGS(depthStencilBuffer.GetAddressOf()));
+
+			depthStencilBuffer->SetName(L"Depth Stencil Buffer");
+
+			D3D12_DEPTH_STENCIL_VIEW_DESC depthStencilDesc = {};
+			depthStencilDesc.Format = DXGI_FORMAT_D32_FLOAT;
+			depthStencilDesc.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2D;
+			depthStencilDesc.Flags = D3D12_DSV_FLAG_NONE;
+
+			device->CreateDepthStencilView(depthStencilBuffer.Get(), &depthStencilDesc, dsvHeap->GetCPUDescriptorHandleForHeapStart());
+		}
+
+		virtual void ReleaseSwapChainResources() override {
+			depthStencilBuffer.Reset();
+			dsvHeap.Reset();
+			Egg::App::ReleaseSwapChainResources();
 		}
 
 		virtual void Resize(int width, int height) override {
